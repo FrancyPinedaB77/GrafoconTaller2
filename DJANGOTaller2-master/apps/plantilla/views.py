403204e43075f9de7f37e8b85b44c58357fd453f
FRANCY <francypineda@gmail.com>
2016-09-25 22:18:54 -0500

    hace filtro para 2 relaciones

--- DJANGOTaller2-master/apps/plantilla/views.py
+++ DJANGOTaller2-master/apps/plantilla/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.http import JsonResponse
-from .forms import NameForm
 import os
 #HACIENDO UN SUBPROCESO
 
 #TERMINANDO EL SUBPROCESO
 
-graph = {"nodes": [], "edges": []}
+
 os.system ("scp cluster_bigdata:lasalida/salidam* /home/estudiante/GrafoconTaller2/DJANGOTaller2-master")
 #def archive (request)
 def inicio(request):
     return render(request, 'inicio.html')
 
-def findNodeId(nodeLabel):
+def findNodeId(nodeLabel,graph):
     #graph = {"nodes": [], "edges": []}
     nodes = graph["nodes"]
     for n in nodes:
@@ -29,6 +28,7 @@ def grafo(request):
     return render(request, "grafo.html")
 
 def mygraph(request):
+    graph = {"nodes": [], "edges": []}
     file = open("data.txt")
     a=request.GET['country']
     b=request.GET['name']
@@ -39,10 +39,9 @@ def mygraph(request):
         values= line.split(",")
          #INCIO DEL FILTRO PARA BUSCAR POR NOMBRE
          #FIN DEL FILTRO PARA BUSCAR POR NOMBRE
-
         fromLabel= values[0]
         toLabel= values[2]
-        from_id=findNodeId(fromLabel)
+        from_id=findNodeId(fromLabel, graph)
         if (fromLabel==b or toLabel==a): #ESTE HACE EL FILTRO POR PAIS
         #if fromLabel== "Shakira" : # ESTE HACE EL FILTRO POR NOMBRE
             if from_id==-1:
@@ -51,7 +50,7 @@ def mygraph(request):
                 from_id=node_id
                 node_id=node_id + 1
 
-            to_id=findNodeId(toLabel)
+            to_id=findNodeId(toLabel, graph)
             if to_id==-1:
                 nodes= graph["nodes"]
                 nodes.append({"id": node_id, "label": toLabel})
